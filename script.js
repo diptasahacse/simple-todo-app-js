ade4412af51393687ebf5db74115336ea6323dd8
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-02-19 14:16:40 +0600

    fixed remove button issues

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
 function createList(value) {
-    // let li = document.createElement('li');
-    // li.setAttribute('list-group-item m-1 border rounded d-flex justify-content-between');
-    // let div = document.createElement('div');
-    // div.setAttribute('d-flex');
-    // let tickLogoIcon = document.createElement('span');
-    // tickLogoIcon.setAttribute('me-1 text-primary');
-    // let
-
-
-
-    let listItem = `
-    
-    `;
     let li = document.createElement('li');
     let classesToAdd = ['single-item', 'list-group-item', 'm-1', 'border', 'rounded', 'd-flex', 'justify-content-between'];
     li.classList.add(...classesToAdd);
-
     let innerLi = `
     <div class="d-flex">
                             <span class="me-1 text-primary"><i class="fa-solid fa-circle-check"></i></span>
@@ -34,6 +20,7 @@ function createList(value) {
 document.getElementById('add-button').addEventListener('click', function() {
     let li = createList(document.getElementById('input-field').value);
     document.getElementById('allList').appendChild(li);
+    document.getElementById('input-field').value = '';
 
 });
 
@@ -48,7 +35,26 @@ document.getElementById('add-button').addEventListener('click', function() {
 
 // }
 
-//Add Hover Effect
+//Add Hover Effect on Delete button
+document.getElementById('allList').addEventListener('mousemove', function(event) {
+
+
+    // add border
+    let deleteIconClass = event.target.parentNode.getAttribute('class');
+    let li = event.target.parentNode.parentNode;
+    if (deleteIconClass.includes('delete-button')) {
+        li.classList.add("border-danger");
+    } else {
+        // // Remove Border
+        // let a = event.target.getAttribute('class');
+        // console.log(a);
+        // console.log(a.includes('single-item'));
+    }
+
+
+
+});
+
 // let deleteButton = document.getElementsByClassName('delete-button');
 // for (const deleteBtn of deleteButton) {
 //     deleteBtn.addEventListener('mouseenter', function() {
@@ -61,11 +67,15 @@ document.getElementById('add-button').addEventListener('click', function() {
 //     });
 
 // }
+
+// Delete Button
 document.getElementById('allList').addEventListener('click', function(event) {
     let liClass = event.target.parentNode.parentNode.getAttribute('class');
     let li = event.target.parentNode.parentNode;
-    if (liClass.includes("single-item")) {
-        li.parentNode.removeChild(li);
+    if (event.target.getAttribute('class') != null) {
+        if (liClass.includes("single-item")) {
+            li.parentNode.removeChild(li);
+        }
     }
 
 });
