3b07955c9a055e582a6256353ece9bac9c22901f
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-02-19 12:41:47 +0600

    make delete button functional

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function createList(value) {
     
     `;
     let li = document.createElement('li');
-    let classesToAdd = ['list-group-item', 'm-1', 'border', 'rounded', 'd-flex', 'justify-content-between'];
+    let classesToAdd = ['single-item', 'list-group-item', 'm-1', 'border', 'rounded', 'd-flex', 'justify-content-between'];
     li.classList.add(...classesToAdd);
 
     let innerLi = `
@@ -49,15 +49,23 @@ document.getElementById('add-button').addEventListener('click', function() {
 // }
 
 //Add Hover Effect
-let deleteButton = document.getElementsByClassName('delete-button');
-for (const deleteBtn of deleteButton) {
-    deleteBtn.addEventListener('mouseenter', function() {
-        let targetList = deleteBtn.parentNode;
-        targetList.classList.add("border-danger");
-    });
-    deleteBtn.addEventListener('mouseleave', function() {
-        let targetList = deleteBtn.parentNode;
-        targetList.classList.remove("border-danger");
-    });
+// let deleteButton = document.getElementsByClassName('delete-button');
+// for (const deleteBtn of deleteButton) {
+//     deleteBtn.addEventListener('mouseenter', function() {
+//         let targetList = deleteBtn.parentNode;
+//         targetList.classList.add("border-danger");
+//     });
+//     deleteBtn.addEventListener('mouseleave', function() {
+//         let targetList = deleteBtn.parentNode;
+//         targetList.classList.remove("border-danger");
+//     });
 
-}
+// }
+document.getElementById('allList').addEventListener('click', function(event) {
+    let liClass = event.target.parentNode.parentNode.getAttribute('class');
+    let li = event.target.parentNode.parentNode;
+    if (liClass.includes("single-item")) {
+        li.parentNode.removeChild(li);
+    }
+
+});
